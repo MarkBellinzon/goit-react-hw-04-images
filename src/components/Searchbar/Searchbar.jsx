42e214ebff5af6fd1f49bb1ae96f171fe35704fb
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-12-12 22:07:18 +0200

    searchbar_hook

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,21 +1,16 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import css from './Searchbar.module.css';
 
-export class Searchbar extends Component {
-   state = {
-    query: '',
+export const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const onChangeInput = e => {
+    setQuery(e.currentTarget.value);
   };
 
-  onChangeInput = e => {
-    this.setState({ query: e.currentTarget.value });
-  };
-
-  onSubmitForm = e => {
+  const onSubmitForm = e => {
     e.preventDefault();
-
-    const { onSubmit } = this.props;
-    const { query } = this.state;
 
     if (query.trim() === '') {
       toast.error('Enter a search term.');
@@ -25,27 +20,80 @@ export class Searchbar extends Component {
     onSubmit(query);
   };
 
-  render() {
-    const { query } = this.state;
+  return (
+    <header className={css.header}>
+      <form className={css.form} onSubmit={onSubmitForm}>
+        <button className={css.button} type="submit">
+          Search
+        </button>
 
-    return (
-      <header className={css.header}>
-        <form className={css.form} onSubmit={this.onSubmitForm}>
-          <button className={css.button} type="submit">
-            Search
-          </button>
+        <input
+          className={css.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+          onChange={onChangeInput}
+        />
+      </form>
+    </header>
+  );
+};
 
-          <input
-            className={css.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={query}
-            onChange={this.onChangeInput}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+
+
+
+
+
+// import { Component } from 'react';
+// import { toast } from 'react-toastify';
+// import css from './Searchbar.module.css';
+
+// export class Searchbar extends Component {
+//    state = {
+//     query: '',
+//   };
+
+//   onChangeInput = e => {
+//     this.setState({ query: e.currentTarget.value });
+//   };
+
+//   onSubmitForm = e => {
+//     e.preventDefault();
+
+//     const { onSubmit } = this.props;
+//     const { query } = this.state;
+
+//     if (query.trim() === '') {
+//       toast.error('Enter a search term.');
+//       return;
+//     }
+
+//     onSubmit(query);
+//   };
+
+//   render() {
+//     const { query } = this.state;
+
+//     return (
+//       <header className={css.header}>
+//         <form className={css.form} onSubmit={this.onSubmitForm}>
+//           <button className={css.button} type="submit">
+//             Search
+//           </button>
+
+//           <input
+//             className={css.input}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={query}
+//             onChange={this.onChangeInput}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
